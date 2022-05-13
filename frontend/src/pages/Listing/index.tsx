da082a7f1839842e32d759e72fab48e956b64d75
cristinevsousa <cristinevsousa@gmail.com>
2022-05-13 18:47:24 -0300

    movie cards

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,32 @@
-import Pagination from "components/Navbar/Pagination";
+import MovieCard from "components/MovieCard";
+
+import Pagination from "components/Pagination";
 
 function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Listing;
