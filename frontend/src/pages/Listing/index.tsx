23d2e7a72e13ecb7e4f9f454d4c326a865757368
cristinevsousa <cristinevsousa@gmail.com>
2022-05-15 19:25:23 -0300

    first request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,17 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+  //para teste
+  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+      .then(response => {
+        console.log(response.data);
+      });
+
   return (
     <>
       <Pagination />
